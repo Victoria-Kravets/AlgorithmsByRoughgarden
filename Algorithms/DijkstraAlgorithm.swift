592d0c69fe652d465910e9c244cd1a8d2960f9bc
victoria_kravets <victoriakravets93@gmail.com>
2020-10-08 19:07:03 +0300

    - fix dijkstra algorithm

--- Algorithms/DijkstraAlgorithm.swift
+++ Algorithms/DijkstraAlgorithm.swift
@@ -8,6 +8,53 @@
 
 import Foundation
 
+class Path {
+    var node: Node
+    var edge: Edge?
+    var previousPath: Path?
+    
+    var pathWeight: Int
+    
+    init(node: Node, edge: Edge? = nil, previousPath: Path? = nil) {
+        if let previousPath = previousPath, let edge = edge {
+            self.pathWeight = previousPath.pathWeight + edge.lenght
+            self.previousPath = previousPath
+            self.edge = edge
+        } else {
+            self.pathWeight = 0
+        }
+        self.node = node
+    }
+}
+
+func dijkstraSearch(sourceNode: Node, finishNode: Node) -> Path? {
+    
+    var paths = [Path]() {
+        didSet {
+            paths.sort(by: { $0.pathWeight < $1.pathWeight })
+        }
+    }
+    
+    paths.append(Path(node: sourceNode))
+    
+    while !paths.isEmpty {
+        let shortestPath = paths.removeFirst()
+        
+        if shortestPath.node.visited { continue }
+        
+        if shortestPath.node == finishNode {
+            return shortestPath
+        }
+        
+        shortestPath.node.visited = true
+        for edge in shortestPath.node.neighbors {
+            paths.append(Path(node: edge.neighbor, edge: edge, previousPath: shortestPath))
+        }
+    }
+    
+    return nil
+}
+
 func dijkstraSearch(graph: Graph, sourceNode: Node, finishNode: Node) -> Int { // O(*nm*)
     var edges = sourceNode.neighbors
     sourceNode.visited = true
@@ -30,3 +77,4 @@ func dijkstraSearch(graph: Graph, sourceNode: Node, finishNode: Node) -> Int { /
     
     return shortestLength
 }
+
